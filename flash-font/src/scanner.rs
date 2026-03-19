ec2a1ecfbcff45eff610f873ddfa5e8e964c39c9
naganohara-yoshino <naganohara-yoshino@outlook.com>
2026-03-19 18:22:32 +0800

    refactor: scanner

--- flash-font/src/scanner.rs
+++ flash-font/src/scanner.rs
@@ -1,22 +1,29 @@
 use camino::{Utf8Path, Utf8PathBuf};
-use jwalk::{Parallelism, WalkDir};
-use std::time::Duration;
+use walkdir::WalkDir;
 
-pub fn scan_font_directory(font_root: &Utf8Path) -> impl Iterator<Item = Utf8PathBuf> {
-    WalkDir::new(font_root)
-        .parallelism(Parallelism::RayonDefaultPool {
-            busy_timeout: Duration::from_secs(1),
-        })
-        .follow_links(false)
+pub fn scan_font_directory(root: impl AsRef<Utf8Path>) -> impl Iterator<Item = Utf8PathBuf> {
+    WalkDir::new(root.as_ref())
         .into_iter()
         .filter_map(Result::ok)
         .filter(|entry| entry.file_type().is_file())
-        .filter_map(|file_entry| Utf8PathBuf::from_path_buf(file_entry.path()).ok())
-        .filter(|file_path_buf| {
-            file_path_buf.extension().is_some_and(|ext| {
+        .filter_map(|file_entry| Utf8PathBuf::from_path_buf(file_entry.into_path()).ok())
+        .filter(|path| {
+            path.extension().is_some_and(|ext| {
                 ["ttf", "otf", "ttc"]
                     .iter()
-                    .any(|valid_ext| ext.eq_ignore_ascii_case(valid_ext))
+                    .any(|v| ext.eq_ignore_ascii_case(v))
             })
         })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_scan_font_directory() {
+        let root = Utf8Path::new(r"G:\Data\fonts");
+        let paths = scan_font_directory(&root);
+        println!("Found {} fonts", paths.count());
+    }
 }
